c6b657df8da1b37900b63a561f4ea1615d3b9049
jlbuild <juliabuildbot@gmail.com>
2026-05-07 14:02:00 +0000

    LittleCMS_jll build 2.19.0+0

--- src/LittleCMS_jll.jl
+++ src/LittleCMS_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("LittleCMS")
-JLLWrappers.@generate_main_file("LittleCMS", UUID("d3a379c0-f9a3-5b72-a4c0-6bf4d2e8af0f"))
+JLLWrappers.@generate_main_file("LittleCMS", Base.UUID("d3a379c0-f9a3-5b72-a4c0-6bf4d2e8af0f"))
 end  # module LittleCMS_jll
